a27c362ded9bf552f586a672aa3b9cce22354d76
Dan <dan.mok999@yandex.ru>
2025-10-10 22:12:07 +0500

    ServiceDescriptorFactory and Upsert singleton

---
+++ src/Medion.Common/ServiceDescriptorFactory.cs
@@ -0,0 +1,91 @@
+﻿namespace Microsoft.Extensions.DependencyInjection.Medion.Common;
+
+public static class ServiceDescriptorFactory
+{
+    public static ServiceDescriptor Create(
+        ServiceLifetime lifetime,
+        Type serviceType,
+        object? serviceKey = null,
+        Type? implementationType = null,
+        object? implementationInstance = null,
+        Func<IServiceProvider, object>? implementationFactory = null,
+        Func<IServiceProvider, object?, object>? keyedImplementationFactory = null)
+    {
+        var hasKey = serviceKey != null;
+        if (implementationType != null)
+            return hasKey
+                ? new ServiceDescriptor(serviceType, serviceKey,
+                    implementationType, lifetime)
+                : new ServiceDescriptor(serviceType, implementationType,
+                    lifetime);
+        if (implementationInstance != null)
+            return hasKey
+                ? new ServiceDescriptor(serviceType, serviceKey,
+                    implementationInstance)
+                : new ServiceDescriptor(serviceType, implementationInstance);
+        if (implementationFactory != null)
+            return new ServiceDescriptor(serviceType, implementationFactory,
+                lifetime);
+        if (implementationFactory != null)
+            return hasKey
+                ? new ServiceDescriptor(serviceType, serviceKey,
+                    (s, _) => implementationFactory(s), lifetime)
+                : new ServiceDescriptor(serviceType, implementationFactory,
+                    lifetime);
+        if (keyedImplementationFactory != null)
+            return hasKey
+                ? new ServiceDescriptor(serviceType, serviceKey,
+                    keyedImplementationFactory, lifetime)
+                : new ServiceDescriptor(serviceType,
+                    s => keyedImplementationFactory(s, null), lifetime);
+
+        throw new Exception();
+    }
+
+    public static ServiceDescriptor CopyWith(
+        this ServiceDescriptor descriptor,
+        ServiceLifetime? lifetime = null,
+        object? serviceKey = null,
+        Type? serviceType = null,
+        Type? implementationType = null,
+        object? implementationInstance = null,
+        Func<IServiceProvider, object>? implementationFactory = null,
+        Func<IServiceProvider, object?, object>? keyedImplementationFactory = null,
+        bool removeKey = false)
+    {
+        lifetime ??= descriptor.Lifetime;
+        serviceType ??= descriptor.ServiceType;
+        serviceKey ??= descriptor.ServiceKey;
+        if (removeKey)
+            serviceKey = null;
+
+        var replaceImpl = implementationType != null ||
+                          implementationInstance != null ||
+                          implementationFactory != null ||
+                          keyedImplementationFactory != null;
+
+        if (!replaceImpl)
+        {
+            implementationType = descriptor.IsKeyedService
+                ? descriptor.KeyedImplementationType
+                : descriptor.ImplementationType;
+            implementationInstance = descriptor.IsKeyedService
+                ? descriptor.KeyedImplementationInstance
+                : descriptor.ImplementationInstance;
+            if (descriptor.IsKeyedService)
+                keyedImplementationFactory = descriptor.KeyedImplementationFactory;
+            else
+                implementationFactory = descriptor.ImplementationFactory;
+        }
+
+        return Create(
+            lifetime.Value,
+            serviceType,
+            serviceKey,
+            implementationType,
+            implementationInstance,
+            implementationFactory,
+            keyedImplementationFactory
+        );
+    }
+}
---
+++ src/Medion.Upsert/ServiceCollectionExtensions.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Extensions.DependencyInjection.Medion.Common;
+
+namespace Microsoft.Extensions.DependencyInjection.Medion.Upsert;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection UpsertSingleton<TService, TImpl>(this IServiceCollection services)
+        where TService : class
+        where TImpl : class, TService
+    {
+        return services.Upsert(ServiceLifetime.Singleton, typeof(TService), implementationType: typeof(TImpl));
+    }
+
+    public static IServiceCollection UpsertSingleton<TService, TImpl>(
+        this IServiceCollection services,
+        object serviceKey)
+        where TService : class
+        where TImpl : class, TService
+    {
+        return services.Upsert(ServiceLifetime.Singleton, typeof(TService), serviceKey, typeof(TImpl));
+    }
+
+    public static IServiceCollection UpsertSingleton<TService>(
+        this IServiceCollection services,
+        TService implementationInstance)
+        where TService : class
+    {
+        return services.Upsert(
+            ServiceLifetime.Singleton,
+            typeof(TService),
+            implementationInstance: implementationInstance
+        );
+    }
+
+    public static IServiceCollection UpsertSingleton<TService>(
+        this IServiceCollection services,
+        TService implementationInstance,
+        object serviceKey)
+        where TService : class
+    {
+        return services.Upsert(
+            ServiceLifetime.Singleton,
+            typeof(TService),
+            serviceKey,
+            implementationInstance: implementationInstance
+        );
+    }
+
+    public static IServiceCollection Upsert(
+        this IServiceCollection services,
+        ServiceLifetime lifetime,
+        Type serviceType,
+        object? serviceKey = null,
+        Type? implementationType = null,
+        object? implementationInstance = null,
+        Func<IServiceProvider, object>? implementationFactory = null,
+        Func<IServiceProvider, object?, object>? keyedImplementationFactory = null)
+    {
+        var upsertIndices = FindIndices(services, serviceType, serviceKey);
+        var serviceUpsert = ServiceDescriptorFactory.Create(
+            lifetime,
+            serviceType,
+            serviceKey,
+            implementationType,
+            implementationInstance,
+            implementationFactory,
+            keyedImplementationFactory
+        );
+
+        if (upsertIndices.Length > 0)
+            foreach (var upsertIndex in upsertIndices)
+                services[upsertIndex] = serviceUpsert;
+        else
+            services.Add(serviceUpsert);
+
+        return services;
+    }
+
+    private static int[] FindIndices(
+        IServiceCollection services,
+        Type serviceType,
+        object? serviceKey = null)
+    {
+        return services
+            .Select((s, i) => (service: s, Index: i))
+            .Where(x => x.service.ServiceType == serviceType && x.service.ServiceKey == serviceKey)
+            .Select(x => x.Index)
+            .ToArray();
+    }
+}
