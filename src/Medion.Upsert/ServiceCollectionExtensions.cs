5154ece270a1d226652b92d4687e98bc956d0377
Dan <dan.mok999@yandex.ru>
2025-10-29 22:28:48 +0500

    Upsert XML doc

--- src/Medion.Upsert/ServiceCollectionExtensions.cs
+++ src/Medion.Upsert/ServiceCollectionExtensions.cs
@@ -4,6 +4,14 @@ namespace Microsoft.Extensions.DependencyInjection.Medion.Upsert;
 
 public static class ServiceCollectionExtensions
 {
+    /// <summary>
+    ///     Adds or replaces all occurrences of <typeparamref name="TService" />
+    ///     in provided service collection with Singleton lifetime
+    /// </summary>
+    /// <typeparam name="TService">Service type to replace</typeparam>
+    /// <typeparam name="TImpl">Replacing type</typeparam>
+    /// <param name="services">Service collection</param>
+    /// <returns>The modified service collection</returns>
     public static IServiceCollection UpsertSingleton<TService, TImpl>(this IServiceCollection services)
         where TService : class
         where TImpl : class, TService
@@ -11,7 +19,16 @@ public static class ServiceCollectionExtensions
         return services.Upsert(ServiceLifetime.Singleton, typeof(TService), implementationType: typeof(TImpl));
     }
 
-    public static IServiceCollection UpsertSingleton<TService, TImpl>(
+    /// <summary>
+    ///     Adds or replaces all occurrences of <typeparamref name="TService" />
+    ///     in provided service collection with Singleton lifetime and specified key
+    /// </summary>
+    /// <typeparam name="TService">Service type to replace</typeparam>
+    /// <typeparam name="TImpl">Replacing type</typeparam>
+    /// <param name="services">Service collection</param>
+    /// <param name="serviceKey">Service key</param>
+    /// <returns>The modified service collection</returns>
+    public static IServiceCollection UpsertKeyedSingleton<TService, TImpl>(
         this IServiceCollection services,
         object serviceKey)
         where TService : class
@@ -20,6 +37,14 @@ public static class ServiceCollectionExtensions
         return services.Upsert(ServiceLifetime.Singleton, typeof(TService), serviceKey, typeof(TImpl));
     }
 
+    /// <summary>
+    ///     Adds or replaces all occurrences of <typeparamref name="TService" />
+    ///     in provided service collection with Singleton lifetime using implementation instance
+    /// </summary>
+    /// <typeparam name="TService">Service type to replace</typeparam>
+    /// <param name="services">Service collection</param>
+    /// <param name="implementationInstance">Implementation instance</param>
+    /// <returns>The modified service collection</returns>
     public static IServiceCollection UpsertSingleton<TService>(
         this IServiceCollection services,
         TService implementationInstance)
@@ -32,7 +57,16 @@ public static class ServiceCollectionExtensions
         );
     }
 
-    public static IServiceCollection UpsertSingleton<TService>(
+    /// <summary>
+    ///     Adds or replaces all occurrences of <typeparamref name="TService" />
+    ///     in provided service collection with Singleton lifetime using implementation instance and specified key
+    /// </summary>
+    /// <typeparam name="TService">Service type to replace</typeparam>
+    /// <param name="services">Service collection</param>
+    /// <param name="implementationInstance">Implementation instance</param>
+    /// <param name="serviceKey">Service key</param>
+    /// <returns>The modified service collection</returns>
+    public static IServiceCollection UpsertKeyedSingleton<TService>(
         this IServiceCollection services,
         TService implementationInstance,
         object serviceKey)
@@ -46,6 +80,213 @@ public static class ServiceCollectionExtensions
         );
     }
 
+    /// <summary>
+    ///     Adds or replaces all occurrences of <typeparamref name="TService" />
+    ///     in provided service collection with Singleton lifetime using implementation factory
+    /// </summary>
+    /// <typeparam name="TService">Service type to replace</typeparam>
+    /// <param name="services">Service collection</param>
+    /// <param name="implementationFactory">Implementation factory</param>
+    /// <returns>The modified service collection</returns>
+    public static IServiceCollection UpsertSingleton<TService>(
+        this IServiceCollection services,
+        Func<IServiceProvider, TService> implementationFactory)
+        where TService : class
+    {
+        return services.Upsert(
+            ServiceLifetime.Singleton,
+            typeof(TService),
+            implementationFactory: implementationFactory
+        );
+    }
+
+    /// <summary>
+    ///     Adds or replaces all occurrences of <typeparamref name="TService" />
+    ///     in provided service collection with Singleton lifetime using implementation factory and specified key
+    /// </summary>
+    /// <typeparam name="TService">Service type to replace</typeparam>
+    /// <param name="services">Service collection</param>
+    /// <param name="keyedImplementationFactory">Keyed implementation factory</param>
+    /// <param name="serviceKey">Service key</param>
+    /// <returns>The modified service collection</returns>
+    public static IServiceCollection UpsertKeyedSingleton<TService>(
+        this IServiceCollection services,
+        Func<IServiceProvider, object?, TService> keyedImplementationFactory,
+        object serviceKey)
+        where TService : class
+    {
+        return services.Upsert(
+            ServiceLifetime.Singleton,
+            typeof(TService),
+            serviceKey,
+            keyedImplementationFactory: keyedImplementationFactory
+        );
+    }
+
+    /// <summary>
+    ///     Adds or replaces all occurrences of <typeparamref name="TService" />
+    ///     in provided service collection with Scoped lifetime
+    /// </summary>
+    /// <typeparam name="TService">Service type to replace</typeparam>
+    /// <typeparam name="TImpl">Replacing type</typeparam>
+    /// <param name="services">Service collection</param>
+    /// <returns>The modified service collection</returns>
+    public static IServiceCollection UpsertScoped<TService, TImpl>(this IServiceCollection services)
+        where TService : class
+        where TImpl : class, TService
+    {
+        return services.Upsert(ServiceLifetime.Scoped, typeof(TService), implementationType: typeof(TImpl));
+    }
+
+    /// <summary>
+    ///     Adds or replaces all occurrences of <typeparamref name="TService" />
+    ///     in provided service collection with Scoped lifetime and specified key
+    /// </summary>
+    /// <typeparam name="TService">Service type to replace</typeparam>
+    /// <typeparam name="TImpl">Replacing type</typeparam>
+    /// <param name="services">Service collection</param>
+    /// <param name="serviceKey">Service key</param>
+    /// <returns>The modified service collection</returns>
+    public static IServiceCollection UpsertKeyedScoped<TService, TImpl>(
+        this IServiceCollection services,
+        object serviceKey)
+        where TService : class
+        where TImpl : class, TService
+    {
+        return services.Upsert(ServiceLifetime.Scoped, typeof(TService), serviceKey, typeof(TImpl));
+    }
+
+    /// <summary>
+    ///     Adds or replaces all occurrences of <typeparamref name="TService" />
+    ///     in provided service collection with Scoped lifetime using implementation factory
+    /// </summary>
+    /// <typeparam name="TService">Service type to replace</typeparam>
+    /// <param name="services">Service collection</param>
+    /// <param name="implementationFactory">Implementation factory</param>
+    /// <returns>The modified service collection</returns>
+    public static IServiceCollection UpsertScoped<TService>(
+        this IServiceCollection services,
+        Func<IServiceProvider, TService> implementationFactory)
+        where TService : class
+    {
+        return services.Upsert(
+            ServiceLifetime.Scoped,
+            typeof(TService),
+            implementationFactory: implementationFactory
+        );
+    }
+
+    /// <summary>
+    ///     Adds or replaces all occurrences of <typeparamref name="TService" />
+    ///     in provided service collection with Scoped lifetime using implementation factory and specified key
+    /// </summary>
+    /// <typeparam name="TService">Service type to replace</typeparam>
+    /// <param name="services">Service collection</param>
+    /// <param name="keyedImplementationFactory">Keyed implementation factory</param>
+    /// <param name="serviceKey">Service key</param>
+    /// <returns>The modified service collection</returns>
+    public static IServiceCollection UpsertKeyedScoped<TService>(
+        this IServiceCollection services,
+        Func<IServiceProvider, object?, TService> keyedImplementationFactory,
+        object serviceKey)
+        where TService : class
+    {
+        return services.Upsert(
+            ServiceLifetime.Scoped,
+            typeof(TService),
+            serviceKey,
+            keyedImplementationFactory: keyedImplementationFactory
+        );
+    }
+
+    /// <summary>
+    ///     Adds or replaces all occurrences of <typeparamref name="TService" />
+    ///     in provided service collection with Transient lifetime
+    /// </summary>
+    /// <typeparam name="TService">Service type to replace</typeparam>
+    /// <typeparam name="TImpl">Replacing type</typeparam>
+    /// <param name="services">Service collection</param>
+    /// <returns>The modified service collection</returns>
+    public static IServiceCollection UpsertTransient<TService, TImpl>(this IServiceCollection services)
+        where TService : class
+        where TImpl : class, TService
+    {
+        return services.Upsert(ServiceLifetime.Transient, typeof(TService), implementationType: typeof(TImpl));
+    }
+
+    /// <summary>
+    ///     Adds or replaces all occurrences of <typeparamref name="TService" />
+    ///     in provided service collection with Transient lifetime and specified key
+    /// </summary>
+    /// <typeparam name="TService">Service type to replace</typeparam>
+    /// <typeparam name="TImpl">Replacing type</typeparam>
+    /// <param name="services">Service collection</param>
+    /// <param name="serviceKey">Service key</param>
+    /// <returns>The modified service collection</returns>
+    public static IServiceCollection UpsertKeyedTransient<TService, TImpl>(
+        this IServiceCollection services,
+        object serviceKey)
+        where TService : class
+        where TImpl : class, TService
+    {
+        return services.Upsert(ServiceLifetime.Transient, typeof(TService), serviceKey, typeof(TImpl));
+    }
+
+    /// <summary>
+    ///     Adds or replaces all occurrences of <typeparamref name="TService" />
+    ///     in provided service collection with Transient lifetime using implementation factory
+    /// </summary>
+    /// <typeparam name="TService">Service type to replace</typeparam>
+    /// <param name="services">Service collection</param>
+    /// <param name="implementationFactory">Implementation factory</param>
+    /// <returns>The modified service collection</returns>
+    public static IServiceCollection UpsertTransient<TService>(
+        this IServiceCollection services,
+        Func<IServiceProvider, TService> implementationFactory)
+        where TService : class
+    {
+        return services.Upsert(
+            ServiceLifetime.Transient,
+            typeof(TService),
+            implementationFactory: implementationFactory
+        );
+    }
+
+    /// <summary>
+    ///     Adds or replaces all occurrences of <typeparamref name="TService" />
+    ///     in provided service collection with Transient lifetime using implementation factory and specified key
+    /// </summary>
+    /// <typeparam name="TService">Service type to replace</typeparam>
+    /// <param name="services">Service collection</param>
+    /// <param name="keyedImplementationFactory">Keyed implementation factory</param>
+    /// <param name="serviceKey">Service key</param>
+    /// <returns>The modified service collection</returns>
+    public static IServiceCollection UpsertKeyedTransient<TService>(
+        this IServiceCollection services,
+        Func<IServiceProvider, object?, TService> keyedImplementationFactory,
+        object serviceKey)
+        where TService : class
+    {
+        return services.Upsert(
+            ServiceLifetime.Transient,
+            typeof(TService),
+            serviceKey,
+            keyedImplementationFactory: keyedImplementationFactory
+        );
+    }
+
+    /// <summary>
+    ///     Adds or replaces all occurrences of service type in provided service collection
+    /// </summary>
+    /// <param name="services">Service collection</param>
+    /// <param name="lifetime">Service lifetime</param>
+    /// <param name="serviceType">Service type</param>
+    /// <param name="serviceKey">Service key</param>
+    /// <param name="implementationType">Implementation type</param>
+    /// <param name="implementationInstance">Implementation instance</param>
+    /// <param name="implementationFactory">Implementation factory</param>
+    /// <param name="keyedImplementationFactory">Keyed implementation factory</param>
+    /// <returns>The modified service collection</returns>
     public static IServiceCollection Upsert(
         this IServiceCollection services,
         ServiceLifetime lifetime,
